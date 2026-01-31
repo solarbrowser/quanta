965385b8c088b97a80218f28cb53b92e8840d27c
Ata <68029503+ataturkcu@users.noreply.github.com>
2026-01-31 11:35:01 +0300

    feat: implement compound bitwise assignment operators
    Added support for bitwise assignment operators:
    - LEFT_SHIFT_ASSIGN (<<=)
    - RIGHT_SHIFT_ASSIGN (>>=)
    - UNSIGNED_RIGHT_SHIFT_ASSIGN (>>>=)
    - BITWISE_AND_ASSIGN (&=)
    - BITWISE_OR_ASSIGN (|=)
    - BITWISE_XOR_ASSIGN (^=)

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -1308,7 +1308,13 @@ bool Parser::is_assignment_operator(TokenType type) const {
            type == TokenType::MINUS_ASSIGN ||
            type == TokenType::MULTIPLY_ASSIGN ||
            type == TokenType::DIVIDE_ASSIGN ||
-           type == TokenType::MODULO_ASSIGN;
+           type == TokenType::MODULO_ASSIGN ||
+           type == TokenType::BITWISE_AND_ASSIGN ||
+           type == TokenType::BITWISE_OR_ASSIGN ||
+           type == TokenType::BITWISE_XOR_ASSIGN ||
+           type == TokenType::LEFT_SHIFT_ASSIGN ||
+           type == TokenType::RIGHT_SHIFT_ASSIGN ||
+           type == TokenType::UNSIGNED_RIGHT_SHIFT_ASSIGN;
 }
 
 bool Parser::is_binary_operator(TokenType type) const {
