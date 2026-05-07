cd35d45ad32684f3a040cb5621e7825d79bcd40f
Ata <68029503+ataturkcu@users.noreply.github.com>
2026-05-07 15:48:33 +0300

    refactor(ast): extract statements to seperate file

---
+++ src/parser/ast/ast_internal.h
@@ -0,0 +1,33 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+#include <string>
+#include <cstddef>
+
+namespace Quanta {
+
+inline size_t utf16_length(const std::string& str) {
+    size_t count = 0;
+    for (size_t i = 0; i < str.size(); ) {
+        unsigned char c = (unsigned char)str[i];
+        if (c >= 0xF0) { count += 2; i += 4; }
+        else if (c >= 0xE0) { count += 1; i += 3; }
+        else if (c >= 0xC0) { count += 1; i += 2; }
+        else { count += 1; i += 1; }
+    }
+    return count;
+}
+
+void increment_loop_depth();
+void decrement_loop_depth();
+
+struct LoopDepthGuard {
+    LoopDepthGuard() { increment_loop_depth(); }
+    ~LoopDepthGuard() { decrement_loop_depth(); }
+};
+
+} // namespace Quanta
